4ef1eeb1a0a4310f7b0f6608bdc8714a0028c240
aliazamx21 <aliazamgo754@gmail.com>
2026-08-11 13:11:47 +0530

    fixing ec2 instance name

--- terraform/main.tf
+++ terraform/main.tf
@@ -154,6 +154,11 @@ resource "aws_eks_node_group" "nodes" {
 
   instance_types = ["t3.large"]
 
+  # Added proper naming tags so EC2 instances display cleanly in the AWS Console
+  tags = {
+    Name = "hotelbooking-worker-node"
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.nodes_AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.nodes_AmazonEKS_CNI_Policy,
