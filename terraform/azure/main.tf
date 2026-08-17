2a1e341af20f72b68f46e8b8144420daf59d5392
aliazamx21 <aliazamgo754@gmail.com>
2026-08-17 11:48:54 +0530

    fix: revert AKS vm_size to Standard_D2s_v5 for hotel booking architecture

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -19,23 +19,22 @@ provider "azurerm" {
   features {}
 }
 
-# 1. Resource Group
 resource "azurerm_resource_group" "devops_rg" {
   name     = "hotel-devops-rg"
   location = "Central India"
 }
 
-# 2. Azure Kubernetes Service (AKS) for Hotel DevOps Tools
 resource "azurerm_kubernetes_cluster" "devops_aks" {
   name                = "hotel-devops-aks"
   location            = azurerm_resource_group.devops_rg.location
   resource_group_name = azurerm_resource_group.devops_rg.name
   dns_prefix          = "hotel-devops-aks"
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name       = "devopspool"
     node_count = 2
-    vm_size    = "Standard_D2s_v3"
+    vm_size    = "Standard_D2s_v5" # <-- Reverted to your known-working VM size
   }
 
   identity {
@@ -48,10 +47,9 @@ resource "azurerm_kubernetes_cluster" "devops_aks" {
   }
 }
 
-# --- OUTPUTS ---
 output "kubernetes_cluster_name" {
   value       = azurerm_kubernetes_cluster.devops_aks.name
-  description = "The name of the Azure AKS Cluster for Hotel Booking"
+  description = "The name of the Azure AKS Cluster"
 }
 
 output "resource_group_name" {
